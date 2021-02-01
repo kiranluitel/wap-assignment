f985915c3b877b19b99e70e13f0df03f2ec37d6f
kiranluitel <52364043+kiranluitel@users.noreply.github.com>
2021-01-31 21:28:42 -0500

    ajax home work

---
+++ ajax/script.js
@@ -0,0 +1,26 @@
+$(function () {
+    'use strict';
+    $("#sendBtn").on("click", sendRequest);
+    function sendRequest(evt) {
+
+        $.ajax("https://jsonplaceholder.typicode.com/posts",
+            {
+                type: "get",
+                dataType: "json",
+                data: { "userId": $("#idx").val() }
+            }
+        ).done(function (result) {
+
+            const $ul = $("<ul>")[0];
+            for (let i = 0; i < result.length; i++) {
+                const $li = $("<li>")[0];
+                $li.innerText = "Title : " + result[i].title + " \n" + " Post : " + result[i].body;
+                $ul.append($li);
+            }
+            $("#posts").append($ul);
+
+        }).fail(function (err) {
+            alert("failed : " + err.statusText);
+        });
+    }
+});
